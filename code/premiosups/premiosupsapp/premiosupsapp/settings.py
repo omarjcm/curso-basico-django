817c50db9664ad27e7dbb0de48128599cca0dd48
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-01-26 17:57:32 -0500

    Creacion del modelo de polls.

--- code/premiosups/premiosupsapp/premiosupsapp/settings.py
+++ code/premiosups/premiosupsapp/premiosupsapp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'premiosupsapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'premiosups',
+        'USER': 'postgres',
+        'PASSWORD': 'inexcelsisdeo',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Guayaquil'
 
 USE_I18N = True
 
--- readme.txt
+++ readme.txt
@@ -11,4 +11,10 @@ python manage.py runserver
 
 python manage.py startapp polls
 
+Listado de la base de datos
+https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 
+pip install psycopg2
+
+python manage.py makemigrations polls
+python manage.py migrate

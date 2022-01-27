5e00c8a7ab57cc7becf6c5e876c563a683d7fa9f
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-01-27 16:20:04 -0500

    Presentacion de los detalles de una pregunta.

--- code/premiosups/premiosupsapp/polls/views.py
+++ code/premiosups/premiosupsapp/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question
 
@@ -8,7 +8,8 @@ def index(request):
     return render(request, 'polls/index.html', { 'latest_question_list': latest_question_list })
     
 def detail(request, question_id):
-    return HttpResponse(f'Estas viendo la pregunta numero {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', { 'question': question })
 
 def results(request, question_id):
     return HttpResponse(f'Estas viendo los resultados de la pregunta numero {question_id}')
